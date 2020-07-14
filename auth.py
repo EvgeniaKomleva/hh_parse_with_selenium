b9892476ec25d54463250455af5efbd254b4a520
EvgeniaKomleva <komleva.1999@inbox.ru>
2020-07-14 14:16:49 +0300

    Parse work

--- auth.py
+++ auth.py
@@ -1,17 +1,39 @@
-import script
 import time
+import io
+from selenium import webdriver
+from bs4 import BeautifulSoup as soup
 
-driver.get('https://hh.ru/account/login?from=employer_index_header&backurl=%2Femployer')
-# This gets the element name i.e your username field.
-# We’ll learn how to do this later (If you are curious)
-username = driver.find_element_by_name('username')
-# Now we click that ,virtually ofcourse. username.click()
-# send_keys() is like typing words on the element
-username.send_keys('komelva.1999@inbox.ru')
-# Same goes for Password Field
-password = driver.find_element_by_name('password')
-password.click()
-password.send_keys ('test')
-# click on submit button
-username.submit()
-sleep(300)
+auth_status = 0
+options = ''
+
+myurl = 'https://hh.ru/search/resume?area=1&clusters=true&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=full_text&search_period=1&specialization=1&text=&experience=noExperience'
+#myurl = 'https://hh.ru/search/resume?text=&st=resumeSearch&logic=normal&pos=full_text&exp_period=all_time&exp_company_size=any&exp_industry=any&specialization=1&area=1&relocation=living_or_relocation&salary_from=&salary_to=&currency_code=RUR&experience=noExperience&education=none&age_from=&age_to=&gender=unknown&order_by=relevance&search_period=1&items_on_page=1'
+driver = webdriver.Chrome(executable_path='C:/ProgramData/chocolatey/lib/chromedriver/tools/chromedriver.exe',
+                          chrome_options=options)
+
+driver.get(myurl)
+time.sleep(1)
+
+filename = "data/data.csv"
+f = io.open(filename, "w", encoding="utf-8")
+headers = "title,href,last_work_place\n"
+f.write(headers)
+
+all_resume = driver.find_elements_by_xpath('//div[@class="resume-search-item__content"]')
+
+
+for resume in all_resume:
+    title_conteiner = resume.find_element_by_xpath('.//div[@class="resume-search-item__header"]')
+    title = title_conteiner.find_element_by_xpath('.//span[@class="bloko-section-header-3 bloko-section-header-3_lite"]').text
+    href_contein = title_conteiner.find_element_by_xpath('.//span[@class="bloko-section-header-3 bloko-section-header-3_lite"]/a')
+    href = href_contein.get_attribute('href')
+    last_work = ''
+    try:
+        last_work = resume.find_element_by_xpath('.//span[@class="resume-search-item__company-name"]').text
+    except:
+        last_work = 'None'
+    f.write(str(title).replace(',', ' ') + "," + str(href) + "," + str(last_work).replace(',', ' ') + "\n")
+
+f.close()
+
+# driver.close()
--- script.py
+++ script.py
@@ -5,7 +5,7 @@ import json
 from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
-from bs4 import BeautifulSoup as soup  # grab page
+from bs4 import BeautifulSoup as soup
 import os
 from selenium.webdriver.chrome.options import Options
 
@@ -16,29 +16,17 @@ if auth_status == 1:
     user_name = 'komle'  # впишите ваше имя компьютера
     profile_path = r'C:\Users\{}\AppData\Local\Google\Chrome\User Data'.format(user_name)
     options.add_argument("user-data-dir={}".format(profile_path))
-
-myurl = 'https://hh.ru/search/resume?area=1&clusters=true&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=full_text&search_period=1&specialization=1&text=&experience=noExperience'
-# myurl = 'https://hh.ru/search/resume?area=1&clusters=true&exp_period=all_time&experience=noExperience&logic=normal&no_magic=false&order_by=relevance&pos=full_text&search_period=1&text='
+myurl = 'https://hh.ru/search/resume?area=1&clusters=true&exp_period=all_time&experience=noExperience&label=only_with_salary&logic=normal&no_magic=false&order_by=relevance&pos=full_text&search_period=1&specialization=1&text=&salary_from=25000&salary_to=40000'
+#myurl = 'https://hh.ru/search/resume?area=1&clusters=true&exp_period=all_time&logic=normal&no_magic=false&order_by=relevance&pos=full_text&search_period=1&specialization=1&text=&experience=noExperience'
 driver = webdriver.Chrome(executable_path='C:/ProgramData/chocolatey/lib/chromedriver/tools/chromedriver.exe',
                           chrome_options=options)
-# myurl = 'https://hh.ru/search/resume?area=1&clusters=true&exp_period=all_time&experience=noExperience&label=only_with_salary&logic=normal&no_magic=false&order_by=relevance&pos=full_text&search_period=1&text=&salary_from=25000&salary_to=40000'
-# AUTH!!!!!!!!!!!!!!!!!!!!!!!!
 
-# driver.get('https://hh.ru/account/login?from=employer_index_header&backurl=%2Femployer')
-# username = driver.find_element_by_name('username')
-# username.send_keys('razilyakomleva@mail.ru')
-# password = driver.find_element_by_name('password')
-# password.click()
-# password.send_keys('test')
-# username.submit()
-
-# sleep(300)
-# time.sleep(300)
 driver.get(myurl)
 time.sleep(1)
 
 page_soup = soup(driver.page_source, "html.parser")
-resume = page_soup.findAll("div", {"data-qa": "resume-serp__resume"})  # получаем блок с резюме
+#resume = page_soup.findAll("div", {"data-qa": "resume-serp__resume"})  # получаем блок с резюме
+all_resume = driver.find_elements_by_xpath('//div[@class="resume-search-item__content"]')
 
 filename = "data/data.csv"
 f = io.open(filename, "w", encoding="utf-8")
@@ -46,7 +34,7 @@ headers = "title,href,last_work_place\n"
 f.write(headers)
 
 buttons = page_soup.findAll("span", {"class": "pager-item-not-in-short-range"})
-last_page = buttons[-1].text
+last_page = 3#buttons[-1].text
 print(last_page)  # получаем количество страниц для парсинга
 
 i = 0
@@ -54,19 +42,19 @@ j = 0
 
 while i < int(last_page):
     i = i + 1  # номер текущей страницы парсинга
-    # print(i)
-    # page_soup = soup(driver.page_source, "html.parser")  # получаем резюме с текущей страницы
-    # resume = page_soup.findAll("div", {"data-qa": "resume-serp__resume"})
 
-    for res in resume:
-        j = j + 1
-        title = res.find("span", {"class": "bloko-section-header-3 bloko-section-header-3_lite"}).text
-        href = res.find("a").get('href')
-        last_work_Container = res.select('span.resume-search-item__company-name')
-        last_work = 'null'
-        for work in last_work_Container:
-            last_work_text = work.get_text(strip=True)
-            last_work = last_work_text
+    for resume in all_resume:
+        title_conteiner = resume.find_element_by_xpath('.//div[@class="resume-search-item__header"]')
+        title = title_conteiner.find_element_by_xpath(
+            './/span[@class="bloko-section-header-3 bloko-section-header-3_lite"]').text
+        href_contein = title_conteiner.find_element_by_xpath(
+            './/span[@class="bloko-section-header-3 bloko-section-header-3_lite"]/a')
+        href = href_contein.get_attribute('href')
+        last_work = ''
+        try:
+            last_work = resume.find_element_by_xpath('.//span[@class="resume-search-item__company-name"]').text
+        except:
+            last_work = 'None'
         f.write(str(title).replace(',', ' ') + "," + str(href) + "," + str(last_work).replace(',', ' ') + "\n")
 
     # if i < int(last_page)-1:
@@ -77,38 +65,43 @@ while i < int(last_page):
     k = 0
     try:
         k = k + 1
-        page = driver.find_element_by_xpath(
-            '//a[@data-qa="pager-next"]')  # Для перехода на след страницу (кроме последней!)
+        page = driver.find_element_by_xpath('//a[@data-qa="pager-next"]')  # Для перехода на след страницу (кроме последней!)
         driver.implicitly_wait(5)
         page.click()
         driver.implicitly_wait(5)
         # print("OK")
     except:
-        print(i)
+        print('cant parse{}'.format(i))
 
     driver.implicitly_wait(5)
     page_soup = soup(driver.page_source, "html.parser")  # получаем резюме с текущей страницы
-    resume = page_soup.findAll("div", {"data-qa": "resume-serp__resume"})
-    if (len(resume) == 0):
+    #resume = page_soup.findAll("div", {"data-qa": "resume-serp__resume"})
+    all_resume = driver.find_elements_by_xpath('//div[@class="resume-search-item__content"]')
+
+    if (len(all_resume) == 0):
         print('AAAAAAAAAAAAAAAAA')
-        resume = page_soup.findAll("div", {"data-qa": "resume-serp__results-search"})
-        print(i, resume)
+        #resume = driver.find_elements_by_xpath('//div[@data-qa="resume-serp__results-search"]')
+        #resume = page_soup.findAll("div", {"data-qa": "resume_search_result"})
+
+        #page_soup = soup(resume.page_source, "html.parser")
+        print(i)
     # print(resume)                             resume-serp__results-search resume-serp__results-search
     driver.implicitly_wait(5)
 
 print(k, j, i, last_page)
 
 # для последней страницы
-for res in resume:
-    j = j + 1
-    title = res.find("span", {"class": "bloko-section-header-3 bloko-section-header-3_lite"}).text
-    href = res.find("a").get('href')
-    last_work_Container = res.select('span.resume-search-item__company-name')
-    last_work = 'null'
-    for work in last_work_Container:
-        last_work_text = work.get_text(strip=True)
-        last_work = last_work_text
-    f.write(str(title).replace(',', ' ') + "," + str(href) + "," + str(last_work).replace(',', ' ') + "\n")
+# for resume in all_resume:
+#     title_conteiner = resume.find_element_by_xpath('.//div[@class="resume-search-item__header"]')
+#     title = title_conteiner.find_element_by_xpath('.//span[@class="bloko-section-header-3 bloko-section-header-3_lite"]').text
+#     href_contein = title_conteiner.find_element_by_xpath('.//span[@class="bloko-section-header-3 bloko-section-header-3_lite"]/a')
+#     href = href_contein.get_attribute('href')
+#     last_work = ''
+#     try:
+#         last_work = resume.find_element_by_xpath('.//span[@class="resume-search-item__company-name"]').text
+#     except:
+#         last_work = 'None'
+#     f.write(str(title).replace(',', ' ') + "," + str(href) + "," + str(last_work).replace(',', ' ') + "\n")
 
 f.close()
 
